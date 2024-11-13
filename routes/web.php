736da6997c6ea0c1693ab291cd5eb87ef1dc6477
rajesh <developerrajeshgupta@gmail.com>
2024-11-13 13:26:23 +0530

    update route file

--- routes/web.php
+++ routes/web.php
@@ -8,18 +8,17 @@ Route::get('/', function () {
 
 Auth::routes();
 
-//Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-
 Route::get('/admin', function () {
     return view('admin.index');
 })->middleware('auth');
 
-Route::get('/loandetails',[App\Http\Controllers\LoanDetailsController::class,'index']);
-Route::get('/emidetails',[App\Http\Controllers\EmiDetailsController::class,'index']);
-Route::post('/loanprocess',[App\Http\Controllers\LoanDetailsController::class,'processData'])->name('loan.processData');
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/loandetails',[App\Http\Controllers\LoanDetailsController::class,'index']);
+Route::post('/loanprocess',[App\Http\Controllers\LoanDetailsController::class,'processData'])->name('loan.processData');
 Route::get('/loanprocess',[App\Http\Controllers\LoanDetailsController::class,'processDataView']);
+
+Route::get('/emidetails',[App\Http\Controllers\EmiDetailsController::class,'index']);
 
 Route::get('/custom-login', [App\Http\Controllers\Auth\LoginController::class, 'loginView']);
 Route::post('/custom-login', [App\Http\Controllers\Auth\LoginController::class, 'login'])->name('custom-login');
